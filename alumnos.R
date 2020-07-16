ae84e2d996858f93d5428822ee8e471ab9bcd47e
MaurickThom <thomtwd@gmail.com>
2020-07-16 13:50:04 -0500

    references

--- alumnos.R
+++ alumnos.R
@@ -1,23 +1,28 @@
 library(readxl)
 #file.choose()
 # leer excel ???
+# https://rpubs.com/osoramirez/93049
+# https://bookdown.org/jboscomendoza/r-principiantes4/graficas-de-barras.html
+
 table <- read_excel("C:\\Users\\User\\Desktop\\informe\\alumnos.xlsx",sheet="Reporte")
 column.age <- as.vector(table$`Edad`)
 column.note <- as.vector(table$`Nota`)
 
-# calcular la media de las edades  ???
+# calcular la media aritmetica de las edades
 print(mean(column.age))
 
-# calcular la media de las notas  ???
+# calcular la media aritmeticade las notas
 mean(mean(column.note))
 
+min(column.age)
+min(column.note)
 
 
 
 View(table)
 attach(table)
 names(table)
-plot(`Edad` ~ `Nota`,main="Diagrama de dispercion entre las variables\n Edad y nota",xlab="edad",ylab="nota")
+plot(`Edad` ~ `Nota`,main="Diagrama de dispercion entre las variables\n Edad y nota",xlab="Nota",ylab="Edad")
 abline(lm(`Edad` ~ `Nota`))
 
 
